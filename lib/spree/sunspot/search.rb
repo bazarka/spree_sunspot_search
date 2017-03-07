a22f26c24c1eda403d77f12c22665076776f94c6
Daniel Niedzialkowski <dniedzialkowski@netkodo.com>
2017-03-07 15:13:38 +0100

    removed per_page

--- lib/spree/sunspot/search.rb
+++ lib/spree/sunspot/search.rb
@@ -62,7 +62,7 @@ module Spree
       def get_base_scope
         base_scope = Spree::Product.active
         base_scope = base_scope.in_taxon(taxon) unless taxon.blank?
-        base_scope = get_products_conditions_for(base_scope, keywords, 30)
+        base_scope = get_products_conditions_for(base_scope, keywords)
         # base_scope = base_scope.on_hand unless Spree::Config[:show_zero_stock_products]
 
         base_scope = add_search_scopes(base_scope)
